7912ea4a7c168131fa37fbe1ea696e2228b3b5ba
Marc Closa <43375758+mclosa8@users.noreply.github.com>
2025-05-27 16:42:10 -0400

    Computer Modern Serif actually freaks out

--- snowwi_tools/utils.py
+++ snowwi_tools/utils.py
@@ -56,7 +56,6 @@ def set_rcParams(plt, tex=False):
     plt.rcParams['figure.dpi'] = 200
     plt.rcParams['font.size'] = 8
     plt.rcParams['font.family'] = 'serif'
-    plt.rcParams['font.serif'] = ['Computer Modern Serif']
     plt.rcParams['lines.linewidth'] = .7
     plt.rcParams["ytick.color"] = "black"
     plt.rcParams["xtick.color"] = "black"
